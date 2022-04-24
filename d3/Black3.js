8f8c77c929fd97f130342ccfc37e0b1f92ab30f1
YiQingK <61070463+YiQingK@users.noreply.github.com>
2022-04-23 22:04:42 -0700

    A complete

--- d3/Black3.js
+++ d3/Black3.js
@@ -1,14 +1,69 @@
 // set the dimensions and margins of the graph
-const margin = {top: 50, right: 30, bottom: 20, left: 50},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const width_b = 600,
+    height_b = 450;
 
 // append the svg object to the body of the page
-const svg = d3.select("#white2")
+const svg_b = d3.select("#black3")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
-
-var container_g = svg
+    .attr("width", width_b)
+    .attr("height", height_b)
     .append("g")
-    .attr("transform",`translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(225, 225)`);
+
+/*Create chart title*/
+svg_b.append("text")
+    .attr("x",-120)
+    .attr("y",-200)
+    .attr("font-size","15px")
+    .text("Number of kills in VALORANT Masters 2021");
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+const radius = 200 ;
+
+// set the color scale
+const color_b = d3.scaleOrdinal()
+    .range(['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928'])
+
+var arc = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var labelArc = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d.K; });
+
+d3.csv("d3/player_stats.csv").then(function(data){
+
+    console.log(pie(data));
+
+    data.sort((a,b)=>d3.ascending(a.Team,b.Team));
+
+    container_b = svg_b.selectAll(".arc")
+        .data(pie(data))
+        .enter()
+        .append("g")
+        .attr("class", "arc");
+
+    container_b.append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color_b(d.data.Team); })
+        .style("stroke",function(d) { return color_b(d.data.Team); });
+
+    svg_b.append("g")
+        .attr("class", "legendQuant")
+        .attr("transform", "translate(200,-200)");
+
+    var legend_b = d3.legendColor()
+        .useClass(false)
+        .title("legend")
+        .titleWidth(100)
+        .scale(color_b);
+
+    svg_b.select(".legendQuant")
+        .call(legend_b);
+})
+
--- d3/White3.js
+++ d3/White3.js
@@ -1,14 +1,71 @@
 // set the dimensions and margins of the graph
-const margin = {top: 50, right: 30, bottom: 20, left: 50},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const margin_w = {top: 50, right: 30, bottom: 50, left: 100},
+    width_w = 550 - margin_w.left - margin_w.right,
+    height_w = 450 - margin_w.top - margin_w.bottom;
 
 // append the svg object to the body of the page
-const svg = d3.select("#white2")
+const svg_w = d3.select("#white3")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
-
-var container_g = svg
+    .attr("width", width_w + margin_w.left + margin_w.right)
+    .attr("height", height_w + margin_w.top + margin_w.bottom)
     .append("g")
-    .attr("transform",`translate(${margin.left},${margin.top})`);
+    .attr("transform",`translate(${margin_w.left},${margin_w.top})`);
+
+/*Create chart title*/
+svg_w.append("text")
+    .attr("x",20)
+    .attr("y",-20)
+    .attr("font-size","15px")
+    .text("Number of kills in VALORANT Masters 2021");
+
+
+var xScale_w = d3.scaleLinear().range([0,width]);
+var yScale_w = d3.scaleBand().range([height,0]);
+
+const mapped_data = new Map();
+
+d3.csv("d3/player_stats.csv").then(function(data){
+
+    data.forEach(function(d,i){
+            if(mapped_data.has(d.Team))
+            {
+                mapped_data.set(d.Team,(mapped_data.get(d.Team)+(+d.K)));
+            }
+            else
+            {
+                mapped_data.set(d.Team,+d.K);
+            }
+    })
+
+    const data_sorted = new Map([...mapped_data.entries()].sort((a, b) => a[1] - b[1]));
+
+    yScale_w.domain(data_sorted.keys());
+    xScale_w.domain([0,1300]);
+
+    var bars = svg_w.selectAll(".bar")
+        .data(data_sorted)
+        .enter()
+        .append("g")
+
+    bars.append("rect")
+        .attr("class","bar")
+        .attr("x",0)
+        .attr("y",function(d){return yScale_w(d[0])+5;})
+        .attr("width",function(d){return xScale_w(d[1]);})
+        .attr("height",20)
+        .attr("fill","#1f78b4");
+
+    svg_w.append("g")
+        .attr("transform", `translate(0, 330)`)
+        .call(d3.axisBottom(xScale_w))
+        .append("text")
+        .attr("x",200)
+        .attr("y",30)
+        .attr("fill", "black")
+        .text("Kills");
+
+    // Add Y axis
+    svg_w.append("g")
+        .call(d3.axisLeft(yScale_w));
+
+});
